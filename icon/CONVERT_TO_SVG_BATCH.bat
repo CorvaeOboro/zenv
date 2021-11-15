1247cb6f6cdb07eac073529f7247e74dce87b507
CorvaeOboro <joethiel@gmail.com>
2021-11-15 04:10:27 -0800

    icon pass update for all active hdas

--- icon/CONVERT_TO_SVG_BATCH.bat
+++
@@ -1,9 +0,0 @@
-rem // REQUIRES IMAGE MAGICK 7.0.10 https://imagemagick.org/script/download.php
-SETLOCAL ENABLEDELAYEDEXPANSION
-for /r %%f in (*.png*) do (
-set  _nametif=%%f
-set  _nametga=!_nametif:.png=.svg!
-set  _finalname=!_nametga:.png=.svg!
-echo !_finalname!
-magick convert "%%f" "!_finalname!"
-)
